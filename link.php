f3d77eae60462f051d6919c5b57f922023207d66
eom <dlstjq1015@naver.com>
2019-10-06 19:39:16 +0900

    1

--- link.php
+++ link.php
@@ -8,7 +8,7 @@ function files($path){
   $files = scandir($path);
   //입력한 폴더에 파일목록을 베열로 반환한다.
   for($i=0;$i<count($files);$i++){
-    if($files[$i]== "" || $files[$i]==".git"){
+    if($files[$i]== ".." || $files[$i]==".git"){
       continue;
     }
     if(is_dir($files[$i])){
